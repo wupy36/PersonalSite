12fdae6d5d6fa92a195a21a786d0aa6ce2a58848
Andrew Duit <wupy36@gmail.com>
2023-10-17 10:01:12 -0500

    Update to images

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,12 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta name='keyword' content='react, unreal engine, ue5, full stack,  nextjs, vr, engineer, full stack, developer'/>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
       <main className='w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar-thin scrollbar-track-textDark/5 scrollbar-thumb-textDark/70' >     
         <Navbar />
